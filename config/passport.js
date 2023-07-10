b466ee93bdb00b270e1bfb40d84cb6c10d7a763b
menth0s <marslite@pm.me>
2023-07-10 10:34:37 -0400

    Index online AIDB

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
+const userModel = require('../models/user');
+const user = require('../models/user');
 
 // configuring Passport!
 passport.use(new GoogleStrategy({
@@ -8,10 +10,31 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  function(accessToken, refreshToken, profile, cb) {
+  async function(accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
 
+    try{
+      //If the user has logged into AIDB we want to ensure that the their information are passed to the middlewae
+      let userDocument = await userModel.findOne({googleId: profile.id});
+      //If no user is found, the user will be undefined
+
+      //If the user is to be found, we pass their info to the next middelware chain
+      if(userDocument) return cb(null, userDocument);
+      //In case that's the first time for the user loggign in, we need to create a user document and store it in our mongodb
+      // and then pass this info to our middlware chain
+
+      //First time Logging in to AIDB
+      userDocument = await userModel.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      });
+      //pass the creates user info to the next function in the middlewae chain
+      return cb(null, userDocument);
+    }catch(err){
+      return cb(err);
+    }
   }
 ));
 
@@ -19,11 +42,18 @@ passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(async function(id, done) {
 
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
+
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
+
+  //Here we are finding the user by getting the if from the session cookie and search the db
+  const user = await userModel.findById(id);
+  done(err, user);
+  
+  
 
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+
 // The root route renders our only view
 router.get('/', function(req, res) {
   //UPDATE THIS
@@ -8,7 +9,11 @@ router.get('/', function(req, res) {
   // in the student demo this was res.redirect('/movies'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
+  // res.redirect('/index');
+  res.render('index', {title: 'AIDB'})
 });
+
+
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
@@ -20,15 +25,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
+    successRedirect : '/index', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect : '/error' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/'). // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/index') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
